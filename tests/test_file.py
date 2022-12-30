8df074bdae87d9bf9a388cf84799c2150b52379e
yanvoida <jan.emil.wojda@gmail.com>
2022-12-31 00:50:25 +0100

    Published.

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,39 +1,20 @@
 import os
-from xlsxopera import Notebook
-
-file = "test.xlsx"
 
 
 class TestFile:
 
-    # @staticmethod
-    # def _active_book():
-    #     return Notebook("test.xlsx")
-
     @staticmethod
-    def _tests_book():
-        return Notebook(file, "tests")
-
-    @staticmethod
-    def _rows_and_cols_book():
-        return Notebook(file, "rows and cols")
-
-    @staticmethod
-    def _math_book():
-        return Notebook(file, "math")
-
-    @staticmethod
-    def test_xlsx_file():
+    def test_xlsx_file(test_file):
         for file_in_dir in os.listdir():
             if file_in_dir.endswith('.xlsx'):
-                assert file_in_dir == file
+                assert file_in_dir == test_file
 
-    def test_tests_sheet_exist(self):
-        assert self._tests_book().headers() == [
+    def test_tests_sheet_exist(self, tests_book):
+        assert tests_book.headers() == [
             'ID', 'Status', 'Test Name', 'Test Description',
         ]
 
-    def test_rows_and_cols_sheet_exist(self):
-        assert self._rows_and_cols_book().headers() == [
+    def test_rows_and_cols_sheet_exist(self, rows_and_cols_book):
+        assert rows_and_cols_book.headers() == [
             'COLUMN A', 'COLUMN B', 'COLUMN C', 'COLUMN D', 'COLUMN E', 'COLUMN F'
         ]
--- tests/test_rows_and_cols.py
+++ tests/test_rows_and_cols.py
@@ -1,18 +1,8 @@
-from xlsxopera import Notebook
-
 
 class TestRowsAndCols:
 
-    @staticmethod
-    def _rows_and_cols_book():
-        return Notebook("test.xlsx", "rows and cols")
-
-    @staticmethod
-    def _data_convert_book():
-        return Notebook("test.xlsx", "data convert")
-
-    def test_get_cols_with_value(self):
-        assert self._rows_and_cols_book().value_cols("John Smith") == [
+    def test_get_cols_with_value(self, rows_and_cols_book):
+        assert rows_and_cols_book.value_cols("John Smith") == [
             [
                 'COLUMN E',
                 'value_1_column_E',
@@ -24,8 +14,8 @@ class TestRowsAndCols:
                 ]
         ]
 
-    def test_get_rows_with_value(self):
-        assert self._rows_and_cols_book().value_rows("value_3_column_F") == [
+    def test_get_rows_with_value(self, rows_and_cols_book):
+        assert rows_and_cols_book.value_rows("value_3_column_F") == [
             [
                 'value_3_column_A',
                 'value_3_column_B',
@@ -36,43 +26,43 @@ class TestRowsAndCols:
                 ]
         ]
 
-    def test_get_cell_coordinates_by_value(self):
-        assert self._rows_and_cols_book().value_coordinates(
+    def test_get_cell_coordinates_by_value(self, rows_and_cols_book):
+        assert rows_and_cols_book.value_coordinates(
             "value_1_column_F"
         ) == (2, 6)
-        assert self._rows_and_cols_book().value_coordinates(
+        assert rows_and_cols_book.value_coordinates(
             "value_3_column_A"
         ) == (4, 1)
 
-    def test_get_cell_position_by_value(self):
-        assert self._rows_and_cols_book().value_position(
+    def test_get_cell_position_by_value(self, rows_and_cols_book):
+        assert rows_and_cols_book.value_position(
             "value_3_column_C"
         ) == "C4"
-        assert self._rows_and_cols_book().value_position(
+        assert rows_and_cols_book.value_position(
             "value_2_column_E"
         ) == "E3"
 
-    def test_find_cell_by_value(self):
-        assert True if self._rows_and_cols_book().find("value_2_column_B") \
-                       and self._rows_and_cols_book().find("Value_that_not_exist") is False else False
+    def test_find_cell_by_value(self, rows_and_cols_book):
+        assert True if rows_and_cols_book.find("value_2_column_B") \
+                       and rows_and_cols_book.find("Value_that_not_exist") is False else False
 
-    def test_get_values_below_the_header(self):
-        assert self._rows_and_cols_book().header_values("COLUMN D") == [
+    def test_get_values_below_the_header(self, rows_and_cols_book):
+        assert rows_and_cols_book.header_values("COLUMN D") == [
             'value_1_column_D', 'value_2_column_D', 'value_3_column_D',
             'value_1_column_D_align_center', 'value_1_column_D_center_vertically',
             'environment'
         ]
-        assert self._rows_and_cols_book().header_values("value_1_column_E_align_center") == [
+        assert rows_and_cols_book.header_values("value_1_column_E_align_center") == [
             'value_1_column_E_center_vertically', 'John Smith'
         ]
 
-    def test_get_headers(self):
-        assert self._rows_and_cols_book().headers() == [
+    def test_get_headers(self, rows_and_cols_book):
+        assert rows_and_cols_book.headers() == [
             "COLUMN A", "COLUMN B", "COLUMN C", "COLUMN D", "COLUMN E", "COLUMN F"
         ]
 
-    def test_get_all_data_converted_to_dict(self):
-        assert self._data_convert_book().data_convert() == {
+    def test_get_all_data_converted_to_dict(self, data_convert_book):
+        assert data_convert_book.data_convert() == {
             "COLUMN A":
                 [
                     'value_1_column_A', 'value_2_column_A', 'value_3_column_A'
@@ -83,11 +73,11 @@ class TestRowsAndCols:
                 ],
         }
 
-    def test_get_cols_2_3_in_dict(self):
+    def test_get_cols_2_3_in_dict(self, rows_and_cols_book):
         """
         Get dict of cols from 2 to 3 and compare.
         """
-        assert self._rows_and_cols_book().dict_cols(start=3, end=4) == {
+        assert rows_and_cols_book.dict_cols(start=3, end=4) == {
             1:
                 [
                     'value_2_column_A', 'value_3_column_A'
@@ -114,11 +104,11 @@ class TestRowsAndCols:
                 ],
         }
 
-    def test_get_cols_2_3_4_in_list(self):
+    def test_get_cols_2_3_4_in_list(self, rows_and_cols_book):
         """
         Get lists of cols from 2 to 4 and compare.
         """
-        assert self._rows_and_cols_book().list_cols(start=2, end=4) == [
+        assert rows_and_cols_book.list_cols(start=2, end=4) == [
             [
                 'value_1_column_A', 'value_2_column_A', 'value_3_column_A'
             ],
@@ -139,11 +129,11 @@ class TestRowsAndCols:
             ]
         ]
 
-    def test_get_rows_2_3_in_dict(self):
+    def test_get_rows_2_3_in_dict(self, rows_and_cols_book):
         """
         Get dict of rows from 2 to 4.
         """
-        assert self._rows_and_cols_book().dict_rows(start=3, end=4) == {
+        assert rows_and_cols_book.dict_rows(start=3, end=4) == {
             3:
             [
                 'value_2_column_A',
@@ -164,11 +154,11 @@ class TestRowsAndCols:
             ]
         }
 
-    def test_get_rows_2_3_4_in_list(self):
+    def test_get_rows_2_3_4_in_list(self, rows_and_cols_book):
         """
         Get lists rows from 2 to 4.
         """
-        assert self._rows_and_cols_book().list_rows(start=2, end=4) == [
+        assert rows_and_cols_book.list_rows(start=2, end=4) == [
             [
                 'value_1_column_A',
                 'value_1_column_B',
@@ -195,24 +185,24 @@ class TestRowsAndCols:
             ]
         ]
 
-    def test_get_data_in_list_by_columns(self):
+    def test_get_data_in_list_by_columns(self, rows_and_cols_book):
         """
         Get first 2 values and last 2 values in sheet. Listing by columns get first rows from position A1 and A2.
         Same logic for last 2 values.
         """
-        assert True if self._rows_and_cols_book().data_cols()[0:2] == [
+        assert True if rows_and_cols_book.data_cols()[0:2] == [
             'COLUMN A', 'value_1_column_A'
-        ] and self._rows_and_cols_book().data_cols()[-2::] == [
+        ] and rows_and_cols_book.data_cols()[-2::] == [
             'value_1_column_F_center_vertically', None
         ] else False
 
-    def test_get_data_in_list_by_rows(self):
+    def test_get_data_in_list_by_rows(self, rows_and_cols_book):
         """
         Get first 2 values and last 2 values in sheet. Listing by rows get first rows from position A1 and B1.
         Same logic for last 2 values.
         """
-        assert True if self._rows_and_cols_book().data_rows()[0:2] == [
+        assert True if rows_and_cols_book.data_rows()[0:2] == [
             'COLUMN A', 'COLUMN B'
-        ] and self._rows_and_cols_book().data_rows()[-2::] == [
+        ] and rows_and_cols_book.data_rows()[-2::] == [
             'John Smith', None
         ] else False
